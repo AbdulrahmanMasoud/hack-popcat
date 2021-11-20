6c647344dafc46e46b9961331eb9cb3a2c239d81
Abdulrahman Masoud <abdulrahman.masoud.official@gmail.com>
2021-11-20 14:30:28 +0200

    Update popcat.js

--- popcat.js
+++ popcat.js
@@ -1,5 +1,6 @@
 setInterval(function(numOfClick = 1000){
-for (i = 0; i < numOfClick; i++) {
- document.dispatchEvent(new KeyboardEvent('keydown', {shiftKey:true}));
-}
+    for (i = 0; i < numOfClick; i++) {
+     document.dispatchEvent(new KeyboardEvent('keydown', {shiftKey:true}));
+    }
+});
   
